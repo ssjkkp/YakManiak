e5d9585137577c6623eac2b85c3de33b1c6b83b2
Sami Kankaanpää <ssjkkp@gmail.com>
2026-01-06 23:36:02 +0200

    .Net maui project template files

---
+++ YakManiak/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace YakManiak
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ YakManiak/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "YakManiak")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "YakManiak.Pages")]
